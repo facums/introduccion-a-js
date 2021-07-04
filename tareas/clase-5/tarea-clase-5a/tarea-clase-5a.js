ff79c0f5944522b8d86901d0d0862ed53fec2f17
Facundo Magallanes <facundomagallanes97@gmail.com>
2021-07-04 16:48:30 -0300

    Comentarios de la tarea

--- tareas/clase-5/tarea-clase-5a/tarea-clase-5a.js
+++ tareas/clase-5/tarea-clase-5a/tarea-clase-5a.js
@@ -1,6 +1,15 @@
+//TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
+// creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
+// también vamos a crear un <h1> que diga Bienvenido!
+// vas a crear un botón de acción que una vez que lo apretás, va a
+// mostrar toda la información junta en un campo de texto
+// Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
+
+
 const $buttonSubmit = document.querySelector('#button-submit');
 const $buttonShow = document.querySelector('#button-show');
 const userIds = ['#user-first-name', '#user-second-name', '#user-surname', '#user-age'];
+
 $buttonSubmit.onclick = function() {
 
     const userData = userIds.map(function(id){   
