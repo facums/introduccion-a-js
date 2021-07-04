7468310e5f21ce5fe2e658c9d464cc53fd44ab05
Facundo Magallanes <facundomagallanes97@gmail.com>
2021-07-04 16:45:24 -0300

    Toma los valores que escribio el user en las entradas

--- tareas/clase-5/tarea-clase-5a/tarea-clase-5a.js
+++ tareas/clase-5/tarea-clase-5a/tarea-clase-5a.js
@@ -3,6 +3,9 @@ const $buttonShow = document.querySelector('#button-show');
 const userIds = ['#user-first-name', '#user-second-name', '#user-surname', '#user-age'];
 $buttonSubmit.onclick = function() {
 
+    const userData = userIds.map(function(id){   
+        return document.querySelector(id).value;
+    });
 
     
 
