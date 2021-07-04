4211998cf9762eace55d2824ea89b9eceb3674f7
Facundo Magallanes <facundomagallanes97@gmail.com>
2021-07-04 16:42:06 -0300

    Crea un evento para el boton "submit"

--- tareas/clase-5/tarea-clase-5a/tarea-clase-5a.js
+++ tareas/clase-5/tarea-clase-5a/tarea-clase-5a.js
@@ -1,3 +1,10 @@
 const $buttonSubmit = document.querySelector('#button-submit');
 const $buttonShow = document.querySelector('#button-show');
 const userIds = ['#user-first-name', '#user-second-name', '#user-surname', '#user-age'];
+$buttonSubmit.onclick = function() {
+
+
+    
+
+    return false;
+}
