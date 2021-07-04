1b89d7e7b7b933370f5ef022e5fdaee789e0568a
Facundo Magallanes <facundomagallanes97@gmail.com>
2021-07-04 16:40:58 -0300

    Crea un arreglo con los ids de las entradas

--- tareas/clase-5/tarea-clase-5a/tarea-clase-5a.js
+++ tareas/clase-5/tarea-clase-5a/tarea-clase-5a.js
@@ -1,2 +1,3 @@
 const $buttonSubmit = document.querySelector('#button-submit');
 const $buttonShow = document.querySelector('#button-show');
+const userIds = ['#user-first-name', '#user-second-name', '#user-surname', '#user-age'];
