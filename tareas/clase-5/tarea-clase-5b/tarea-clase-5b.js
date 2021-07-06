eaac926494d658ca385574462858d17c4ebcf6a1
Facundo Magallanes <facundomagallanes97@gmail.com>
2021-07-06 17:50:26 -0300

    Crea variables para los elementos de los botones

---
+++ tareas/clase-5/tarea-clase-5b/tarea-clase-5b.js
@@ -0,0 +1,6 @@
+const $addButton = document.querySelector('#add-button');
+const $deleteButton = document.querySelector('#delete-button');
+const $submitButton = document.querySelector('#submit-button');
+const $resetButton = document.querySelector('#reset-button');
+
+
