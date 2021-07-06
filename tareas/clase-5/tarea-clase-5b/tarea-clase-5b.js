7f1a7cd9d6cc5ca6515691cb84cf56fab703d6e5
Facundo Magallanes <facundomagallanes97@gmail.com>
2021-07-06 17:57:09 -0300

    Crea un evento para el boton 'agregar'

--- tareas/clase-5/tarea-clase-5b/tarea-clase-5b.js
+++ tareas/clase-5/tarea-clase-5b/tarea-clase-5b.js
@@ -4,3 +4,20 @@ const $submitButton = document.querySelector('#submit-button');
 const $resetButton = document.querySelector('#reset-button');
 
 
+$addButton.onclick = function(){
+
+    const $template = document.querySelector('.template');
+    let $clone = $template.cloneNode(true);             // create clone node
+    let $aux = $clone.getElementsByTagName('input');  // auxiliar variable for all inputs within <form>
+
+
+    for(let i=0; i < $aux.length; i++){ // set fields default, because to clone with function cloneNode() copies the inside content
+        $aux[i].value = '';
+    }
+    
+    document.querySelector('.time-calculator-form').appendChild($clone);    // append to <form>
+  
+    return false;
+}
+
+
