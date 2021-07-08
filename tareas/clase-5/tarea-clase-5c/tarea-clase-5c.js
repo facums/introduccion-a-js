3fe4dc815be6d02020a015c92208c1f4383a49ba
Facundo Magallanes <facundomagallanes97@gmail.com>
2021-07-08 01:04:12 -0300

    Crea funcion quickSort

--- tareas/clase-5/tarea-clase-5c/tarea-clase-5c.js
+++ tareas/clase-5/tarea-clase-5c/tarea-clase-5c.js
@@ -5,3 +5,23 @@ function getRandomInteger(min,max){
     return parseInt((Math.random() * (max - min + 1)), 10) + min;
 }
 
+function quickSort(array){
+    if(array.length == 0){
+        return [];
+    }
+
+    let left = [];
+    let right = [];
+    let pivot = array[0];
+
+    for(let i=1; i < array.length; i++){
+        if(array[i] <= pivot){
+            left.push(array[i]);
+        }else{
+            right.push(array[i]);
+        }
+    }
+
+    return [].concat(quickSort(left), pivot, quickSort(right));
+}
+
