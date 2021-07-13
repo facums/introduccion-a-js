76ca2dccc2d6a3428044241b90df8b31512729ba
Facundo Magallanes <facundomagallanes97@gmail.com>
2021-07-13 11:13:19 -0300

    Agrega comentarios

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -1,5 +1,5 @@
 /*
-TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+TAREA (a): Empezar preguntando cuánta gente hay en el grupo familiar.
 Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
 Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
 
@@ -8,7 +8,7 @@ Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuev
 
 
 /*
-TAREA:
+TAREA (b):
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
 Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
 
